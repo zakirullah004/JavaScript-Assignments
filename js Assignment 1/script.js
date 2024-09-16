90f37b23e3cf7a3ed3c9684131bbba36536b11ff
zakir ullah <zk743087@gmail.com>
2024-09-17 00:29:16 +0500

    some more changes

--- js Assignment 1/script.js
+++ js Assignment 1/script.js
@@ -1,13 +1,34 @@
 // chapter 1
+
+  //? question ==> 1
+// 1. Write a script to greet your website visitor using JS alert
+// box.
 alert("Welcome!") 
 
+
+  //? question ==> 2
+//   Write a script to display following message on your web
+// page:
 alert("Error! Please enter a valid password")
 
+
+ //? question ==> 3
+
+// Write a script to display following message on your web
+// page: (Hint : Use line break)
 alert("Welcome to JS land \nHappy coding")
 
+
+ //? question ==> 4
+// 4. Write a script to display following messages in sequence:
 alert("Welcome to JS Land");
 alert("Happy Coding!")
 
+
+ //? question ==> 5 
+
+// Generate the following message through browser’s
+// developer console:
 console.log("Hello... I can run JS through my web browser's console");
 
 // ----------------------------------

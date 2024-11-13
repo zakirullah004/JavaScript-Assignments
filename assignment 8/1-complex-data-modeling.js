80f2dd8c1cbf257aa71df3e6e553775ee775b607
zakir ullah <zk743087@gmail.com>
2024-11-14 00:12:45 +0500

    part 1 Complete

--- assignment 8/1-complex-data-modeling.js
+++ assignment 8/1-complex-data-modeling.js
@@ -107,8 +107,14 @@ let products = [
   },
 ];
 
-// 1- For each product print the variation having minimum
+//! 1- For each product print the variation having minimum
 // Find the variation with the minimum price for each product
+
+
+// ------------------- solution ----------------------------------
+
+// let minimumPriceVariation = products.map(product => product.varations.reduce((preVal,currVal) => preVal.price < currVal.price ? preVal : currVal));
+// console.log(minimumPriceVariation);
 
 // sample output
 // [
@@ -117,13 +123,31 @@ let products = [
 //   { id: 2, color: 'red', price: 50000, quantity: 1 }
 // ]
 
-// 2- Count the number of reviews on each product.
+//! 2- Count the number of reviews on each product.
 
-// 3- Count the average rating of each product.
+// products.forEach(el => {
+//   console.log(el.title,"total reviews =",el.reviews.length);
+// });
 
-// 4- In each product show reviews that need to be moderated (status = false) along with id and title only
+
+//! 3- Count the average rating of each product.
+
+// let avgRating = products.map(product => product.reviews.map(prdRating => prdRating.rating).reduce((pre,curr) => (pre + curr)) / product.reviews.length);
+// console.log(avgRating);
+
+
+
+//! 4- In each product show reviews that need to be moderated (status = false) along with id and title only
 // meanseach product will have id, title and array of unmoderated reviews.
 
+// let FalsyReviews = products.map(product => {
+//   return {
+//     id:product.id,
+//     title:product.title,
+//     reviews:product.reviews.filter(prdReview => !prdReview.status)
+//   }
+// })
+// console.log(FalsyReviews);
 // sample output
 // [
 //   {

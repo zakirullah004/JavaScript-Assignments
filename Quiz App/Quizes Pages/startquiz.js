b7a87a79dfa311c7b8e1102497ec44b61869623e
zakir ullah <zk743087@gmail.com>
2024-12-13 15:44:10 +0500

    start quiz done

--- Quiz App/Quizes Pages/startquiz.js
+++ Quiz App/Quizes Pages/startquiz.js
@@ -17,6 +17,41 @@ startQuizBtn.addEventListener('click',function(e){
     location.assign("quizWindow.html")
 });
 
+
+//!-----------DArk Theme
+
+let body = document.getElementById('body')
+function lightBg(){
+    localStorage.setItem('bg',"light-bg")
+    applyTheme()
+}
+function darkBg(){
+    localStorage.setItem('bg',"dark-bg")
+    applyTheme()
+}
+
+function applyTheme(){
+    let themeColour = localStorage.getItem('bg')
+    if(themeColour == "light-bg"){
+        body.className = "light-bg"
+    }else{
+        body.className = "dark-bg";
+        quizName.style.color = "#f2f2f2"
+    }
+}
+
+
+function setByDefault() {
+    var checkModeState = localStorage.getItem("bg");
+    if (checkModeState === null) {
+      localStorage.setItem("bg","light");
+      applyTheme();
+    } else {
+        applyTheme();
+    }
+}
+
+
 let logoutBtn = document.getElementById('logoutBtn');
 logoutBtn.addEventListener('click',function(){
     location.assign("../index.html")
@@ -26,3 +61,6 @@ logoutBtn.addEventListener('click',function(){
 function goBack(){
     location.assign("../selectQuiz.html");
 }
+
+
+window.onload = setByDefault();

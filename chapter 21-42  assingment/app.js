df39cb50ff7920e8aba81faa2fa6fcc0cde3a555
zakir ullah <zk743087@gmail.com>
2025-08-30 00:14:04 +0500

    21 to 25 done

---
+++ chapter 21-42  assingment/app.js
@@ -0,0 +1,95 @@
+
+
+//! ***********  Chapter 21 to 25   ******************
+
+// question 1
+
+// let firstName = prompt("Enter your first name:")
+// let lastName = prompt("Enter your Last name:")
+// let fullName = `hello! ${firstName} ${lastName}`
+// console.log(fullName);
+
+// question 2
+
+// let phoneModel = prompt("Enter your favourite mobile Name:")
+// let phoneLength = phoneModel.trim().length
+// console.log(phoneModel,"length is->",phoneLength);
+
+
+// question 3
+
+// let citizen = "Pakistani";
+// console.log(citizen);
+// console.log("Index of n:",citizen.indexOf("n"));
+
+
+// question 4
+// let greet = "Hello World";
+// console.log(greet);
+// console.log(greet.lastIndexOf("l"));
+
+
+// question5
+
+// let iAm = "Pakistani";
+// console.log(iAm);
+// console.log("3rd index Character is",iAm.charAt(3));
+
+
+
+// question 6
+//question 1 again using concat method
+
+// let firstName = prompt("Enter your first name:")
+// let lastName = prompt("Enter your Last name:")
+// let fullName = firstName.concat(lastName)
+// console.log("Welcome",fullName);
+
+
+// question 7
+// let city = "Hyderabad";
+// console.log("Heydarabad");
+// console.log("After Replacing",city.replace("Hyder","Islam"));
+
+// question 8
+
+// let messege = "ali and sami are best friends.They play cricket and football together."
+// console.log("Before:",messege);
+// console.log("After:",messege.replaceAll("and","&"));
+
+
+// question 9
+
+// let num = "472";
+// console.log(num);
+// console.log(typeof num);
+
+// let finalNum = parseInt(num)
+// let finalNum = Number(num)
+// let finalNum = +num
+// console.log(finalNum);
+// console.log(typeof finalNum);
+
+
+// question 10
+
+// let userData = prompt("Enter some text");
+// console.log("User Input",userData);
+// console.log("UpperCase",userData.toUpperCase());
+
+
+// question 11
+
+// let userInp = prompt("Enter some text:");
+// console.log("user Input:",userInp);
+// let titleCase = userInp.charAt(0).toUpperCase() + userInp.slice(1)
+// console.log("Title Case:",titleCase);
+
+
+// question 12
+
+// let num = 35.36
+
+// let newNum = num.toString();
+// console.log(newNum.replaceAll(".",""));
+// console.log(typeof newNum);
